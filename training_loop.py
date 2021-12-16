ee2d33e533de38466b3306da2844a96107becb97
noambenmoshe <b.noam@campus.technion.ac.il>
2021-12-16 13:54:38 +0200

    fixed encoder model and started training loop

--- training_loop.py
+++ training_loop.py
@@ -1,10 +1,25 @@
+import torch
+import torch.nn as nn
 
 # train function
+def train(encoder,m_encoder, train_loader, epochs=20, lr=0.001, momentum=0.9):
 
+    loss_fn = nn.CrossEntropyLoss
+
+    #the optimization is done only to the encoder weights and not to the momentum encoder
+    optimizer = torch.optim.SGD(encoder.parameters(), lr=lr, momentum=momentum)
+
+    for epoch in range(epochs):
+        print(f'start epoch {epoch}')
+
+        for x in train_loader:
+            #TODO call augmentaion function
+
+            x = encoder.forward(x)
 
 # evalutate function top-1 accuracy with linear evaluation
 
-#data loaders function
+# data loaders function
 
 # save model
 
